d10b5a121681fb81a7135b50b2aba20c4efd9a7f
the-king-12 <76212053+the-king-12@users.noreply.github.com>
2025-06-23 16:34:10 +0800

    Add files via upload

---
+++ shana/Shana project2.js
@@ -0,0 +1,544 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [];
+
+
+(lib.AnMovieClip = function(){
+	this.actionFrames = [];
+	this.ignorePause = false;
+	this.gotoAndPlay = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
+	}
+	this.play = function(){
+		cjs.MovieClip.prototype.play.call(this);
+	}
+	this.gotoAndStop = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
+	}
+	this.stop = function(){
+		cjs.MovieClip.prototype.stop.call(this);
+	}
+}).prototype = p = new cjs.MovieClip();
+// symbols:
+
+
+
+(lib.pexelsfelixmittermeier957024 = function() {
+	this.initialize(img.pexelsfelixmittermeier957024);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,5979,3986);
+
+
+(lib.pexelspandamemessnow214889126231670762 = function() {
+	this.initialize(img.pexelspandamemessnow214889126231670762);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,6192,4128);
+
+
+(lib.pexelsveeterzy38136 = function() {
+	this.initialize(img.pexelsveeterzy38136);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,5760,3840);
+
+
+(lib.website_button = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.text = new cjs.Text("website", "40px 'Viner Hand ITC'", "#006633");
+	this.text.lineHeight = 67;
+	this.text.lineWidth = 191;
+	this.text.parent = this;
+	this.text.setTransform(25,2);
+
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#000000").s().p("AvEGQIAAsfIeJAAIAAMfg");
+	this.shape.setTransform(96.5,40);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#006633").s().p("AvEGQIAAsfIeJAAIAAMfg");
+	this.shape_1.setTransform(96.5,40);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape},{t:this.text,p:{color:"#006633"}}]}).to({state:[{t:this.shape_1},{t:this.text,p:{color:"#000000"}}]},1).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,218,80);
+
+
+(lib.Tween5 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib.pexelspandamemessnow214889126231670762();
+	this.instance.setTransform(-133,-173,0.0491,0.0556);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-133,-173,303.8,229.6);
+
+
+(lib.Tween4 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib.pexelspandamemessnow214889126231670762();
+	this.instance.setTransform(-88.9,-69.75,0.0287,0.0338);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-88.9,-69.7,177.8,139.5);
+
+
+(lib.Tween3 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib.pexelspandamemessnow214889126231670762();
+	this.instance.setTransform(-88.9,-69.75,0.0287,0.0338);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-88.9,-69.7,177.8,139.5);
+
+
+(lib.replay_button = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.text = new cjs.Text("replay", "40px 'Viner Hand ITC'", "#006633");
+	this.text.lineHeight = 67;
+	this.text.lineWidth = 145;
+	this.text.parent = this;
+	this.text.setTransform(20,2);
+
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#000000").s().p("AuhFFIAAqJIdDAAIAAKJg");
+	this.shape.setTransform(93,32.5);
+
+	this.text_1 = new cjs.Text("replay", "40px 'Viner Hand ITC'");
+	this.text_1.lineHeight = 67;
+	this.text_1.lineWidth = 145;
+	this.text_1.parent = this;
+	this.text_1.setTransform(20,2);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#006633").s().p("AuhFFIAAqJIdDAAIAAKJg");
+	this.shape_1.setTransform(93,32.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape},{t:this.text,p:{x:20,y:2,text:"replay",font:"40px 'Viner Hand ITC'",color:"#006633",lineWidth:145}}]}).to({state:[{t:this.shape_1},{t:this.text_1},{t:this.text,p:{x:348,y:108.05,text:"",font:"40px 'VinerHandITC'",color:"#000000",lineWidth:100}}]},1).wait(3));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,450,174.6);
+
+
+(lib.Next = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// timeline functions:
+	this.frame_0 = function() {
+		playSound("dawnofchange");
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(4));
+
+	// next_button
+	this.text = new cjs.Text("Next", "40px 'Viner Hand ITC'", "#006600");
+	this.text.lineHeight = 67;
+	this.text.lineWidth = 209;
+	this.text.parent = this;
+	this.text.setTransform(31,12);
+
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#000000").s().p("ArUExIAAphIWpAAIAAJhg");
+	this.shape.setTransform(72.5,30.5);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#006633").s().p("ArUExIAAphIWpAAIAAJhg");
+	this.shape_1.setTransform(72.5,30.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape},{t:this.text,p:{x:31,y:12,color:"#006600"}}]}).to({state:[{t:this.shape_1},{t:this.text,p:{x:19,y:2,color:"#000000"}}]},1).to({state:[{t:this.shape},{t:this.text,p:{x:19,y:2,color:"#00CC66"}}]},1).wait(2));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,242,145.1);
+
+
+// stage content:
+(lib.Shanaproject2 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	this.actionFrames = [23,49];
+	// timeline functions:
+	this.frame_23 = function() {
+		/* Stop at This Frame
+		The  timeline will stop/pause at the frame where you insert this code.
+		Can also be used to stop/pause the timeline of movieclips.
+		*/
+		
+		this.stop();
+		
+		/* Click to Go to Frame and Play
+		Clicking on the specified symbol instance moves the playhead to the specified frame in the timeline and continues playback from that frame.
+		Can be used on the main timeline or on movie clip timelines.
+		
+		Instructions:
+		1. Replace the number 5 in the code below with the frame number you would like the playhead to move to when the symbol instance is clicked.
+		2.Frame numbers in EaselJS start at 0 instead of 1
+		*/
+		
+		this.next.addEventListener("click", fl_ClickToGoToAndPlayFromFrame_6.bind(this));
+		
+		function fl_ClickToGoToAndPlayFromFrame_6()
+		{
+			this.gotoAndPlay(25);
+		}
+	}
+	this.frame_49 = function() {
+		/* Stop at This Frame
+		The  timeline will stop/pause at the frame where you insert this code.
+		Can also be used to stop/pause the timeline of movieclips.
+		*/
+		
+		this.stop();
+		
+		/* Click to Go to Frame and Play
+		Clicking on the specified symbol instance moves the playhead to the specified frame in the timeline and continues playback from that frame.
+		Can be used on the main timeline or on movie clip timelines.
+		
+		Instructions:
+		1. Replace the number 5 in the code below with the frame number you would like the playhead to move to when the symbol instance is clicked.
+		2.Frame numbers in EaselJS start at 0 instead of 1
+		*/
+		
+		this.replay_button.addEventListener("click", fl_ClickToGoToAndPlayFromFrame_7.bind(this));
+		
+		function fl_ClickToGoToAndPlayFromFrame_7()
+		{
+			this.gotoAndPlay(5);
+		}
+		
+		
+		
+		/* Click to Go to Web Page
+		Clicking on the specified symbol instance loads the URL in a new browser window.
+		
+		Instructions:
+		1. Replace http://www.adobe.com with the desired URL address.
+		   Keep the quotation marks ("").
+		*/
+		
+		this.website_button.addEventListener("click", fl_ClickToGoToWebPage_9);
+		
+		function fl_ClickToGoToWebPage_9() {
+			window.open("https://www.mandai.com/en.html", "_blank");
+		}
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).wait(23).call(this.frame_23).wait(26).call(this.frame_49).wait(1));
+
+	// Text
+	this.text = new cjs.Text("Welcome to Shana's Zoo", "50px 'Viner Hand ITC'", "#006633");
+	this.text.lineHeight = 83;
+	this.text.lineWidth = 610;
+	this.text.parent = this;
+	this.text.setTransform(135.9,196.95,1,1.0001,0,0.5919,0);
+
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().s("#404040").ss(1,1,1).p("EAm/gAYIAAAxEgm+AAZIAAgx");
+	this.shape.setTransform(423.5,6.55);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#000000").s().p("EgwPAPCIAAgJMBW2AAAIAAAJgEgzeABXIAAwYMBm9AAAIAAQYg");
+	this.shape_1.setTransform(436,272.775);
+
+	this.shape_2 = new cjs.Shape();
+	this.shape_2.graphics.f("#000000").s().p("EgzeAINIAAwZMBm9AAAIAAQZg");
+	this.shape_2.setTransform(441.6,222.525);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape_1},{t:this.shape},{t:this.text,p:{scaleY:1.0001,skewX:0.5919,x:135.9,y:196.95,lineWidth:610}}]}).to({state:[{t:this.shape_2},{t:this.text,p:{scaleY:1,skewX:0,x:157,y:190.4,lineWidth:593}}]},23).to({state:[]},1).wait(26));
+
+	// next
+	this.text_1 = new cjs.Text("Hi everyone! You guys are going to see a panda in the froest .\nBefore that here are the fun fact about pandas, Pandas  front paw is six digits which  it helps  them grip their foods, And in the next page you will see webstie click it to read more information about pandas.", "25px 'Viner Hand ITC'", "#006600");
+	this.text_1.lineHeight = 42;
+	this.text_1.lineWidth = 699;
+	this.text_1.parent = this;
+	this.text_1.setTransform(75,362.05);
+
+	this.next = new lib.Next();
+	this.next.name = "next";
+	this.next.setTransform(10,31.45);
+	new cjs.ButtonHelper(this.next, 0, 1, 2, false, new lib.Next(), 3);
+
+	this.shape_3 = new cjs.Shape();
+	this.shape_3.graphics.f("#000000").s().p("Eg3JARgMAAAgi/MBuTAAAMAAAAi/g");
+	this.shape_3.setTransform(426,463.025);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[]}).to({state:[{t:this.shape_3},{t:this.next},{t:this.text_1}]},23).to({state:[]},1).wait(26));
+
+	// background
+	this.instance = new lib.pexelsfelixmittermeier957024();
+	this.instance.setTransform(-4,-13,0.142,0.1548);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).to({_off:true},24).wait(26));
+
+	// animal
+	this.instance_1 = new lib.Tween3("synched",0);
+	this.instance_1.setTransform(-189.1,429.75);
+	this.instance_1._off = true;
+
+	this.instance_2 = new lib.Tween4("synched",0);
+	this.instance_2.setTransform(144.9,475.75);
+	this.instance_2._off = true;
+
+	this.instance_3 = new lib.Tween5("synched",0);
+	this.instance_3.setTransform(356.25,433.9);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[]}).to({state:[{t:this.instance_1}]},24).to({state:[{t:this.instance_2}]},10).to({state:[{t:this.instance_3}]},10).wait(6));
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(24).to({_off:false},0).to({_off:true,x:144.9,y:475.75},10).wait(16));
+	this.timeline.addTween(cjs.Tween.get(this.instance_2).wait(24).to({_off:false},10).to({_off:true,x:356.25,y:433.9},10).wait(6));
+
+	// replay_button
+	this.text_2 = new cjs.Text("     THANK YOU FOR VISTING MY WORK\n                      HAVE A NICE DAY!", "25px 'Viner Hand ITC'", "#006600");
+	this.text_2.lineHeight = 42;
+	this.text_2.lineWidth = 553;
+	this.text_2.parent = this;
+	this.text_2.setTransform(143.05,71.95);
+
+	this.replay_button = new lib.replay_button();
+	this.replay_button.name = "replay_button";
+	this.replay_button.setTransform(704.05,446.15,1,1,0,0,0,93,34.2);
+	new cjs.ButtonHelper(this.replay_button, 0, 1, 2, false, new lib.replay_button(), 3);
+
+	this.shape_4 = new cjs.Shape();
+	this.shape_4.graphics.f("#000000").s().p("EAauAg0IAAgoIdEAAIAAAogEg3xgRVIAAveMBjDAAAIAAPeg");
+	this.shape_4.setTransform(440.05,270.95);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[]}).to({state:[{t:this.shape_4},{t:this.replay_button},{t:this.text_2}]},49).wait(1));
+
+	// website_button
+	this.website_button = new lib.website_button();
+	this.website_button.name = "website_button";
+	this.website_button.setTransform(716,553.4,1,1,0,0,0,109,40);
+	this.website_button._off = true;
+	new cjs.ButtonHelper(this.website_button, 0, 1, 1);
+
+	this.timeline.addTween(cjs.Tween.get(this.website_button).wait(49).to({_off:false},0).wait(1));
+
+	// page2
+	this.instance_4 = new lib.pexelsveeterzy38136();
+	this.instance_4.setTransform(-21,0,0.1477,0.1597);
+	this.instance_4._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_4).wait(24).to({_off:false},0).wait(26));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new lib.AnMovieClip();
+p.nominalBounds = new cjs.Rectangle(122,287,939.0999999999999,326.4);
+// library properties:
+lib.properties = {
+	id: '6B375F4E64E6874191F10E9B3FC5B9AE',
+	width: 800,
+	height: 600,
+	fps: 25,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/pexelsfelixmittermeier957024.jpg?1747283590915", id:"pexelsfelixmittermeier957024"},
+		{src:"images/pexelspandamemessnow214889126231670762.jpg?1747283590915", id:"pexelspandamemessnow214889126231670762"},
+		{src:"images/pexelsveeterzy38136.jpg?1747283590915", id:"pexelsveeterzy38136"},
+		{src:"sounds/dawnofchange.mp3?1747283590915", id:"dawnofchange"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['6B375F4E64E6874191F10E9B3FC5B9AE'] = {
+	getStage: function() { return exportRoot.stage; },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+an.handleSoundStreamOnTick = function(event) {
+	if(!event.paused){
+		var stageChild = stage.getChildAt(0);
+		if(!stageChild.paused || stageChild.ignorePause){
+			stageChild.syncStreamSounds();
+		}
+	}
+}
+an.handleFilterCache = function(event) {
+	if(!event.paused){
+		var target = event.target;
+		if(target){
+			if(target.filterCacheList){
+				for(var index = 0; index < target.filterCacheList.length ; index++){
+					var cacheInst = target.filterCacheList[index];
+					if((cacheInst.startFrame <= target.currentFrame) && (target.currentFrame <= cacheInst.endFrame)){
+						cacheInst.instance.cache(cacheInst.x, cacheInst.y, cacheInst.w, cacheInst.h);
+					}
+				}
+			}
+		}
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
